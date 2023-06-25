7143a14db11d819314cf47727784ae4eb977ed31
Kemane Donfack <kemanedonfack5@gmail.com>
2023-06-25 17:33:59 +0100

    presenetation layer ok

--- main.tf
+++ main.tf
@@ -54,6 +54,13 @@ module "database" {
   database_sg_id     = module.security_group_database.security_group_id
 }
 
+module "security_group_ec2" {
+  source              = "./modules/securitygroup"
+  security_group_name = "ec2-sg"
+  inbound_port        = [22, 80]
+  vpc_id              = module.network.vpc_id
+}
+
 module "alb_backend" {
   source                = "./modules/alb"
   alb_name              = "Backend-ALB"
@@ -61,19 +68,12 @@ module "alb_backend" {
   alb_subnet_ids        = [module.network.public_subnet_ids[0], module.network.public_subnet_ids[1]]
   targetgroup_name      = "Backend-TG"
   vpc_id                = module.network.vpc_id
-  alb_internal = false
+  alb_internal          = false
   healthy_threshold     = 2
   unhealthy_threshold   = 5
   health_check_interval = 30
   health_check_path     = "/api/tutorials"
   health_check_timeout  = 10
-}
-
-module "security_group_ec2" {
-  source              = "./modules/securitygroup"
-  security_group_name = "ec2-sg"
-  inbound_port        = [22, 80]
-  vpc_id              = module.network.vpc_id
 }
 
 module "ec2_backend" {
@@ -82,6 +82,8 @@ module "ec2_backend" {
   ami                  = "ami-08766f81ab52792ce"
   key_name             = "kemane"
   instance_type        = "t3.micro"
+  userdata             = "backend_userdata.tpl"
+  api_url              = null
   dbpassword           = module.database.database_password
   dbuser               = module.database.database_username
   dbendpoint           = module.database.database_endpoint
@@ -96,3 +98,39 @@ module "ec2_backend" {
 
 }
 
+module "alb_frontend" {
+  source                = "./modules/alb"
+  alb_name              = "Frontend-ALB"
+  alb_sg_id             = module.security_group_alb.security_group_id
+  alb_subnet_ids        = [module.network.public_subnet_ids[2], module.network.public_subnet_ids[3]]
+  targetgroup_name      = "Frontend-TG"
+  vpc_id                = module.network.vpc_id
+  alb_internal          = false
+  healthy_threshold     = 2
+  unhealthy_threshold   = 5
+  health_check_interval = 30
+  health_check_path     = "/"
+  health_check_timeout  = 10
+}
+
+module "ec2_frontend" {
+  source               = "./modules/ec2"
+  template_name        = "FrontendTemplate"
+  ami                  = "ami-08766f81ab52792ce"
+  key_name             = "kemane"
+  instance_type        = "t3.micro"
+  userdata             = "frontend_userdata.tpl"
+  api_url              = module.alb_backend.elb_dns_name
+  dbpassword           = null
+  dbuser               = null
+  dbendpoint           = null
+  dbname               = null
+  ec2_sg_id            = module.security_group_ec2.security_group_id
+  public_subnet_ids    = [module.network.public_subnet_ids[2], module.network.public_subnet_ids[3]]
+  asg_name             = "Frontend-ASG"
+  min_size             = 2
+  max_size             = 4
+  desired_capacity     = 2
+  alb_target_group_arn = module.alb_frontend.alb_target_group_arn
+
+}
